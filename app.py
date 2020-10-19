1a050e54676686803e8d62df434fa25464da26d8
Artem LD31D <artem.12m21@gmail.com>
2020-10-19 20:30:58 +0300

    Added login_page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/login/')
 def index():
-	return 'Hello, world!'
+	return render_template('login/index.html')
 
 
 if __name__ == '__main__':
