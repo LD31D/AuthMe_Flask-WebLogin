2bb36114288034e28c57a7b1f3cc760b9c726a7d
Artem LD31D <artem.12m21@gmail.com>
2020-10-24 18:01:31 +0300

    Added working with .env

--- utils/others/config.py
+++ utils/others/config.py
@@ -1,6 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 class Config(object):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///../authme.db'
+	SQLALCHEMY_DATABASE_URI = str(getenv('SQLALCHEMY_DATABASE_URI'))
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
-	SECRET_KEY = 'SECRET_KEY'
+	SECRET_KEY = str(getenv('SECRET_KEY'))
 	
