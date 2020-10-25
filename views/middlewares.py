62c8c0fa51b7f519b772694fa1aab0d761c77271
Artem LD31D <artem.12m21@gmail.com>
2020-10-25 16:58:06 +0200

    :sparkles: All 404 page redirect to index page

--- views/middlewares.py
+++ views/middlewares.py
@@ -8,4 +8,7 @@ def redirect_to_login_page(response):
     if response.status_code == 401:
         return redirect(url_for('login') + '?next=' + request.url)
 
+    elif response.status_code == 404:
+    	return redirect(url_for('index'))
+
     return response
